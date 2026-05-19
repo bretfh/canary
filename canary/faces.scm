850883f4f55529c1763c09d4671ad8872d2da593
bret <bretfhorne@gmail.com>
2026-05-19 17:30:51 -0400

    canary/faces: keyword face constructor, faces macro

--- canary/faces.scm
+++ canary/faces.scm
@@ -1,43 +1,46 @@
 (define-module (canary faces)
   #:use-module (srfi srfi-9)
   #:export (<face>
-            face?
-            make-face
-            face-fg
-            face-bg
-            face-attrs
+            face face?
+            face-fg face-bg face-attrs
             default-faces
             face-table-lookup
-            extend-face-table))
+            extend-face-table
+            faces))
 
 (define-record-type <face>
-  (make-face fg bg attrs)
-  face?
-  (fg face-fg)
-  (bg face-bg)
+  (%face fg bg attrs) face?
+  (fg    face-fg)
+  (bg    face-bg)
   (attrs face-attrs))
 
+(define* (face #:key fg bg (attrs '()))
+  (%face fg bg attrs))
+
 (define default-faces
-  `((default  . ,(make-face #f #f '()))
-    (accent   . ,(make-face "#ff6b9d" #f '(bold)))
-    (dim      . ,(make-face "#666666" #f '()))
-    (muted    . ,(make-face "#888888" #f '()))
-    (error    . ,(make-face "#ff5555" #f '(bold)))
-    (warning  . ,(make-face "#f4c061" #f '()))
-    (info     . ,(make-face "#7cd1e3" #f '()))
-    (success  . ,(make-face "#00ff87" #f '()))
-    (heading  . ,(make-face "#5599ff" #f '(bold)))
-    (link     . ,(make-face "#5599ff" #f '(underline)))
-    (selection . ,(make-face "#ffffff" "#322a44" '()))
-    (cursor   . ,(make-face "#000000" "#ffffff" '()))
-    (placeholder . ,(make-face "#666666" #f '(italic)))))
+  `((default     . ,(face))
+    (accent      . ,(face #:fg "#ff6b9d" #:attrs '(bold)))
+    (dim         . ,(face #:fg "#666666"))
+    (muted       . ,(face #:fg "#888888"))
+    (error       . ,(face #:fg "#ff5555" #:attrs '(bold)))
+    (warning     . ,(face #:fg "#f4c061"))
+    (info        . ,(face #:fg "#7cd1e3"))
+    (success     . ,(face #:fg "#00ff87"))
+    (heading     . ,(face #:fg "#5599ff" #:attrs '(bold)))
+    (link        . ,(face #:fg "#5599ff" #:attrs '(underline)))
+    (selection   . ,(face #:fg "#ffffff" #:bg "#322a44"))
+    (cursor      . ,(face #:fg "#000000" #:bg "#ffffff"))
+    (placeholder . ,(face #:fg "#666666" #:attrs '(italic)))))
 
 (define (face-table-lookup table name)
   (cond
    ((face? name) name)
-   ((not name) (face-table-lookup table 'default))
+   ((not name)   (face-table-lookup table 'default))
    ((assq name table) => cdr)
    (else (face-table-lookup table 'default))))
 
 (define (extend-face-table base overrides)
   (append overrides base))
+
+(define-syntax-rule (faces (name expr) ...)
+  (list (cons 'name expr) ...))
